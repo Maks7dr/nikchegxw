340788a31d1ccefa68737d170ff1ac79f28275e0
Maksym Drachenko <maks7d@gmail.com>
2024-12-28 20:18:53 +0500

    1000s

--- js/overlay.js
+++ js/overlay.js
@@ -1,23 +1,3 @@
-// document.getElementById('musicButton').addEventListener('click', function () {
-//   const audio = document.getElementById('jingleBells');
-//   const backdrop = document.getElementById('backdrop');
-
-//   // Запускаем музыку
-//   audio
-//     .play()
-//     .then(() => {
-//       // Скрываем оверлей после успешного запуска музыки
-//       backdrop.classList.add('hidden');
-//       // Включаем прокрутку
-//       document.body.style.overflow = '';
-//     })
-//     .catch(error => {
-//       console.error('Музыка не смогла воспроизвестись:', error);
-//     });
-// });
-
-// // Отключаем прокрутку при загрузке бэкдропа
-// document.body.style.overflow = 'hidden';
 document.getElementById('musicButton').addEventListener('click', function () {
   const audio = document.getElementById('jingleBells');
   const backdrop = document.getElementById('backdrop');
