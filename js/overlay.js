7d2f7a0b46d98fc284939bc085e4ac889d323370
Maksym Drachenko <maks7d@gmail.com>
2024-12-28 20:07:15 +0500

    add shake santa

--- js/overlay.js
+++ js/overlay.js
@@ -1,19 +1,48 @@
+// document.getElementById('musicButton').addEventListener('click', function () {
+//   const audio = document.getElementById('jingleBells');
+//   const backdrop = document.getElementById('backdrop');
+
+//   // Запускаем музыку
+//   audio
+//     .play()
+//     .then(() => {
+//       // Скрываем оверлей после успешного запуска музыки
+//       backdrop.classList.add('hidden');
+//       // Включаем прокрутку
+//       document.body.style.overflow = '';
+//     })
+//     .catch(error => {
+//       console.error('Музыка не смогла воспроизвестись:', error);
+//     });
+// });
+
+// // Отключаем прокрутку при загрузке бэкдропа
+// document.body.style.overflow = 'hidden';
 document.getElementById('musicButton').addEventListener('click', function () {
   const audio = document.getElementById('jingleBells');
   const backdrop = document.getElementById('backdrop');
+  const santaButton = document.getElementById('musicButton');
 
-  // Запускаем музыку
-  audio
-    .play()
-    .then(() => {
-      // Скрываем оверлей после успешного запуска музыки
-      backdrop.classList.add('hidden');
-      // Включаем прокрутку
-      document.body.style.overflow = '';
-    })
-    .catch(error => {
-      console.error('Музыка не смогла воспроизвестись:', error);
-    });
+  // Добавляем класс анимации "тряски"
+  santaButton.classList.add('shaking');
+
+  // Ждем 1 секунды перед запуском музыки и скрытием оверлея
+  setTimeout(() => {
+    santaButton.classList.remove('shaking'); // Убираем тряску после завершения
+
+    // Запускаем музыку
+    audio
+      .play()
+      .then(() => {
+        // Скрываем оверлей после успешного запуска музыки
+        backdrop.classList.add('hidden');
+        // Включаем прокрутку
+        document.body.style.overflow = '';
+      })
+      .catch(error => {
+        console.error('Музыка не смогла воспроизвестись:', error);
+      });
+  }, 1000); // Задержка в 1 секунды
 });
 
 // Отключаем прокрутку при загрузке бэкдропа
